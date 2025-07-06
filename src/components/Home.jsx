9e7558495336aae7abe82a105983aa79f6943a6b
Mohamed RUالإيميل المسجل في GitHub! <mr.rossy2020@gmail.com>
2025-07-07 01:11:30 +0300

    add size slider image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,34 +9,26 @@ const Home = () => {
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://www.shutterstock.com/image-vector/3d-shopping-sale-promotion-banner-600nw-2056851833.jpg"
+          className="d-block w-100" style={{height : "400px" , borderRadius : "20px"}}
+          src="https://external-preview.redd.it/gurman-airpods-4-still-coming-this-fall-in-two-new-variants-v0-oJJyX9-k-9zhbZsV9pgmKRrTnbYJPt6bulzW8BK9d08.jpg?width=1080&crop=smart&auto=webp&s=8f52b1f2fc6f9675d3f2aabead5f98826a25a56f"
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100" style={{height : "400px" , borderRadius : "20px"}}
           src="https://tech-offer.com/media/2025/05/Website-msi-v15.jpg"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100" style={{height : "400px" , borderRadius : "20px"}}
           src="https://cdn.shopify.com/s/files/1/2227/7667/files/MSI_Month_Offers.jpg?v=1740561887&width=1400"
           alt="Third slide"
         />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+
       </Carousel.Item>
     </Carousel>
 
